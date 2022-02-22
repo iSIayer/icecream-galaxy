fd018f6fd2e4042eb091e2dc7afe852af4f3993c
Denys07748 <denis07748@gmail.com>
2022-02-22 21:48:33 +0200

    скрывает бургер

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -8,6 +8,7 @@
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
+    openMenuBtn.classList.toggle("is-hidden");
 
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
