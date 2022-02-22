72cccfd9e6f111a1aad5d5ca9484ca65ba34e16d
Denys07748 <denis07748@gmail.com>
2022-02-22 13:10:43 +0200

    убрал полоки на иконке home

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,8 +3,8 @@ import slick from "slick-carousel";
 $('.reviews__slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
     arrows: false, 
     dots: true,
   });
